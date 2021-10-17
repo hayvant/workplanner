17d7e50c4e744959f3b2341a0131e61d7148cfaf
hayvant <89271807+hayvant@users.noreply.github.com>
2021-10-16 20:46:58 -0700

    initial commit on new repo

---
+++ Assets/script.js
@@ -0,0 +1,36 @@
+var button = $(".saveBtn")
+var currentTime = moment().format('MMMM Do YYYY, h:mm a')
+var plannerTime = parseInt($(this).attr('id'))
+
+$('#currentTime').html(currentTime);
+
+function scheduleTime() { 
+    $(".time-block").each(function(){
+        if (plannerTime < currentTime) {
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+        } else if (plannerTime === currentTime) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
+};
+
+  $('.saveBtn').on('click', function () {
+    localStorage.setItem($(this).attr('name'), $('#' + $(this).attr('name')).val());
+  });
+
+  function storage() {
+    for (var key in localStorage) {
+        $('#hour' + key + ' .description').val(localStorage.getItem(key))
+    }
+};
+
+storage();
+scheduleTime();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# workplanner
+## Work Planner
+
+> This is a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+* Add important events to a daily planner
+* Ability to manage time effectively
+* Create your own person schedule
+* Timeblocks are color coded to indicate whether it is in the past, present, or future.
+* Events are saved in local storage
+---------------------------------------------------------------------------------------------------------------------
+
+> Deploy Link: [Daily Work Planner](https://hayvant.github.io/workplanner/)
+
+
+![Portfolio Screenshot]()
+
+
+------------------------------------------------------------
